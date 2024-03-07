49ba9008d10e1197c21f0b00a54c151a8fade339
Mikhail Kamishov <pi_ff@mail.ru>
2024-03-07 10:21:04 +0300

    DZ 4 + mag

--- Seminars/Seminar1/Hero_Classes/Magician.java
+++ Seminars/Seminar1/Hero_Classes/Magician.java
@@ -5,7 +5,18 @@ import Seminar1.Interface.Step;
 import java.util.ArrayList;
 
 public class Magician extends Unit{
-    public Magician(String name,int x, int y, int initiative, int health, int strength, int recovery, int protection, int intelligence, int vitality, int damage) {
+
+    int magarrows;
+
+    public int getMagarrows() {
+        return magarrows;
+    }
+
+    public void setMagarrows(int magarrows) {
+        this.magarrows = magarrows;
+    }
+
+    public Magician(String name, int x, int y, int initiative, int health, int strength, int recovery, int protection, int intelligence, int vitality, int damage) {
         super(name, x, y, initiative, health, strength, recovery, protection, intelligence, vitality, damage);}
 
     public Magician (String name, int x, int y) {
@@ -23,6 +34,21 @@ public class Magician extends Unit{
         System.out.println(this.name + " is summoning a demon.");
     }
 
+    public void Step(ArrayList<Unit> targetTeam) {
+        if (Dead(Magician.this)){
+            if (Dead(nearTarget(targetTeam))){
+                if(Magician.this.getMagarrows() > 0 ){
+                    Magician.this.Attack(nearTarget(targetTeam));
+                    this.magarrows -= 1;
+                    Magician.this.setMagarrows(this.magarrows);
+                    System.out.printf("Cтрел осталось %d\n", getMagarrows());
+                }else{
+                    System.out.println("Нет стрел\n");
+                }
+            }else {targetTeam.remove(nearTarget(targetTeam));}
+        }
 
+
+    }
 
 }
